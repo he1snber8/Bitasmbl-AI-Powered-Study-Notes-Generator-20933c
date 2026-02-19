aa195777c48aeadc539c508eb75823e5b3333c76
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2026-02-19 23:17:36 +0400

    Add Notes.svelte

---
+++ frontend/src/routes/Notes.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+ let content=""; let notes=null;
+ async function generate(){ /* call backend */ }
+</script>
+
+<textarea bind:value={content}></textarea>
+<button on:click={generate}>Generate</button>
+{#if notes}
+ <section></section>
+{/if}
