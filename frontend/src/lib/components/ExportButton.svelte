0a187d2f9013bcfedd0c11f3571a5c51727f8eac
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2026-02-19 23:17:41 +0400

    Add ExportButton.svelte

---
+++ frontend/src/lib/components/ExportButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+ export let noteId:string;
+ async function doExport(format:string){ /* call export API */ }
+</script>
+
+<button on:click={() => doExport("txt")}>Export TXT</button>
+<button on:click={() => doExport("pdf")}>Export PDF</button>
