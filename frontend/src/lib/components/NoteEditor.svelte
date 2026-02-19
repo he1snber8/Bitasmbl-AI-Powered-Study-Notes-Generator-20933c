a23b0f95a62bff5b955988cb165ad6ff7addf4b4
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2026-02-19 23:17:39 +0400

    Add NoteEditor.svelte

---
+++ frontend/src/lib/components/NoteEditor.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+ export let summary="";
+ export let keywords:string[]=[];
+</script>
+
+<div>
+ <p>{summary}</p>
+ <ul>
+  {#each keywords as k}
+   <li>{k}</li>
+  {/each}
+ </ul>
+</div>
